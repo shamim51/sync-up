6425ee635c9bf3bd10c440e891b6660bcda8396a
Shamim Ahmed <shamimahmed30082@gmail.com>
2024-10-09 23:36:30 +0600

    configured db and inital schema file

---
+++ db/schema.ts
@@ -0,0 +1,36 @@
+import { sql } from 'drizzle-orm';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const schedules = sqliteTable('schedules', {
+    id: integer('id').primaryKey(),
+    userId: text('user_id').notNull(),
+    day: text('day').notNull(),
+    timeSlot: text('time_slot').notNull(),
+    isBusy: integer('is_busy').notNull(),
+  });
+
+export const usersTable = sqliteTable('users', {
+  id: integer('id').primaryKey(),
+  name: text('name').notNull(),
+  age: integer('age').notNull(),
+  email: text('email').unique().notNull(),
+});
+
+export const postsTable = sqliteTable('posts', {
+  id: integer('id').primaryKey(),
+  title: text('title').notNull(),
+  content: text('content').notNull(),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+  createdAt: text('created_at')
+    .default(sql`(CURRENT_TIMESTAMP)`)
+    .notNull(),
+  updateAt: integer('updated_at', { mode: 'timestamp' }).$onUpdate(() => new Date()),
+});
+
+export type InsertUser = typeof usersTable.$inferInsert;
+export type SelectUser = typeof usersTable.$inferSelect;
+
+export type InsertPost = typeof postsTable.$inferInsert;
+export type SelectPost = typeof postsTable.$inferSelect;
